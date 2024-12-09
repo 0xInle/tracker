a3f51a2da3b4c70b344106fc633c62ee1e346196
0xInle <xsk8er26x@gmail.com>
2024-12-09 16:47:10 +0300

    Реализация модального окна

--- js/_portfolio.js
+++ js/_portfolio.js
@@ -1,7 +1,19 @@
 let hero = document.getElementById('hero')
 let heroBtn = document.getElementById('heroBtn')
+let portfolio = document.getElementById('portfolio')
+let addTransaction = document.getElementById('addTransaction')
+let modal = document.getElementById('modal')
+let body = document.getElementById('body')
 
-heroBtn.addEventListener('click',() => {
+heroBtn.addEventListener('click', () => {
 
-  hero.innerHTML = ''
+  hero.style.display = 'none'
+  portfolio.style.display = 'block'
+})
+
+addTransaction.addEventListener('click', () => {
+
+  // modal.style.display = 'block'
+  console.log('ckkckd')
+  document.body.classList.add('modal-open');
 })
