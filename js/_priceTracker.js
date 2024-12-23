603b789fa536753af5d4940d44dc05a37caf8f98
0xInle <xsk8er26x@gmail.com>
2024-12-23 20:03:19 +0300

    редактирование функции fetchAndUpdateData

--- js/_priceTracker.js
+++ js/_priceTracker.js
@@ -1,41 +1,49 @@
 // Запрос активов на главную страницу
 const options = { method: 'GET', headers: { accept: 'application/json' } };
 
-function fetchAndUpdateData() {
-  fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20sui%2Csei-network&price_change_percentage=1h&precision=2', options)
-    .then(res => res.json())
-    .then(data => {
-      const btc = data[0];
-      const eth = data[1];
-      const sui = data[2];
-      const sei = data[3];
+async function fetchAndUpdateData() {
+  try {
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20sui%2Csei-network&price_change_percentage=1h&precision=2', options)
 
-      document.getElementById('btc-price').textContent = formatCurrency(btc.current_price);
-      document.getElementById('btc-procent').textContent = formatPercentage(btc.market_cap_change_percentage_24h);
+    if (!response.ok) {
+      throw new Error('Ошибка при получении данных')
+    }
 
-      document.getElementById('eth-price').textContent = formatCurrency(eth.current_price);
-      document.getElementById('eth-procent').textContent = formatPercentage(eth.market_cap_change_percentage_24h);
+    const data = await response.json()
 
-      document.getElementById('sui-price').textContent = formatCurrency(sui.current_price);
-      document.getElementById('sui-procent').textContent = formatPercentage(sui.market_cap_change_percentage_24h);
+    const btc = data[0];
+    const eth = data[1];
+    const sui = data[2];
+    const sei = data[3];
 
-      document.getElementById('sei-price').textContent = formatCurrency(sei.current_price);
-      document.getElementById('sei-procent').textContent = formatPercentage(sei.market_cap_change_percentage_24h);
+    document.getElementById('btc-price').textContent = formatCurrency(btc.current_price);
+    document.getElementById('btc-procent').textContent = formatPercentage(btc.market_cap_change_percentage_24h);
 
-      // Функция изменения цвета для market_cap_change_percentage
-      function changeColor() {
-        const btcPercentage = btc.market_cap_change_percentage_24h;
-        const ethPercentage = eth.market_cap_change_percentage_24h;
-        const suiPercentage = sui.market_cap_change_percentage_24h;
-        const seiPercentage = sei.market_cap_change_percentage_24h;
+    document.getElementById('eth-price').textContent = formatCurrency(eth.current_price);
+    document.getElementById('eth-procent').textContent = formatPercentage(eth.market_cap_change_percentage_24h);
 
-        document.getElementById('btc-procent').style.color = btcPercentage > 0 ? '#46a756' : '#ae3434';
-        document.getElementById('eth-procent').style.color = ethPercentage > 0 ? '#46a756' : '#ae3434';
-        document.getElementById('sui-procent').style.color = suiPercentage > 0 ? '#46a756' : '#ae3434';
-        document.getElementById('sei-procent').style.color = seiPercentage > 0 ? '#46a756' : '#ae3434';
-      }
-      changeColor();
-    })
+    document.getElementById('sui-price').textContent = formatCurrency(sui.current_price);
+    document.getElementById('sui-procent').textContent = formatPercentage(sui.market_cap_change_percentage_24h);
+
+    document.getElementById('sei-price').textContent = formatCurrency(sei.current_price);
+    document.getElementById('sei-procent').textContent = formatPercentage(sei.market_cap_change_percentage_24h);
+
+    // Функция изменения цвета для market_cap_change_percentage
+    function changeColor() {
+      const btcPercentage = btc.market_cap_change_percentage_24h;
+      const ethPercentage = eth.market_cap_change_percentage_24h;
+      const suiPercentage = sui.market_cap_change_percentage_24h;
+      const seiPercentage = sei.market_cap_change_percentage_24h;
+
+      document.getElementById('btc-procent').style.color = btcPercentage > 0 ? '#46a756' : '#ae3434';
+      document.getElementById('eth-procent').style.color = ethPercentage > 0 ? '#46a756' : '#ae3434';
+      document.getElementById('sui-procent').style.color = suiPercentage > 0 ? '#46a756' : '#ae3434';
+      document.getElementById('sei-procent').style.color = seiPercentage > 0 ? '#46a756' : '#ae3434';
+    }
+    changeColor();
+  } catch (error) {
+    console.error('Ошибка:', error.message);
+  };
 }
 
 fetchAndUpdateData()
@@ -51,7 +59,7 @@ function formatCurrency(value) {
   }).format(value).replace(/,/g, ' ') + ' $';
 }
 
-// Функция форматирования процентов
+// // Функция форматирования процентов
 function formatPercentage(value) {
   return value.toFixed(2) + ' %'
 }
